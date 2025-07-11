5d1d71b730c7df2456743eb93947c9dadfba3fe9
Joshua Horsley <24281654+joshhorsley@users.noreply.github.com>
2025-07-11 14:42:00 +1000

    Dropped unused js member code

--- 4_website/src/routes/member-instance.jsx
+++ 4_website/src/routes/member-instance.jsx
@@ -1,21 +1,15 @@
-import {
-    useLoaderData,
-} from "react-router-dom";
-
-import { getMember } from "../members";
+import { useLoaderData } from "react-router-dom";
 
 import { Row, Container } from "react-bootstrap";
 
 import { Link } from "react-router-dom";
 
-import {Popover, OverlayTrigger } from "react-bootstrap";
-
+import { Popover, OverlayTrigger } from "react-bootstrap";
 
 import PageTitle from "../components/PageTitle";
 import CardStats from "../components/cardstats";
 
 import MemberPlot from "../components/MemberPlot";
-
 
 // title
 
@@ -25,18 +19,12 @@ const popover = (
   </Popover>
 );
 
-
-  
 export default function MemberInstance() {
-    const {member} = useLoaderData();
-
-
-
+  const { member } = useLoaderData();
 
   return (
     <>
-    
-    <PageTitle title = {member.name_display ? member.name_display : null}/>
+      <PageTitle title={member.name_display ? member.name_display : null} />
 
       <Container>
         <h1 className="memberTitle">
@@ -51,79 +39,73 @@ export default function MemberInstance() {
 
         <Row>
           {/* <Col> */}
-            <CardStats
-                title = 'Chip'
-                info = 'Most recent chip assigned. No result if chip later reassigned.'
-                value = {member.chipLatest ? member.chipLatest : '-'}
-                />
+          <CardStats
+            title="Chip"
+            info="Most recent chip assigned. No result if chip later reassigned."
+            value={member.chipLatest ? member.chipLatest : "-"}
+          />
           {/* </Col> */}
-          
+
           {/* <Col> */}
+          <CardStats
+            title="Sprint+ Races"
+            info="Includes Sprint (previously Full) and Double Sprint events."
+            value={member.racesFull ? member.racesFull : "-"}
+          >
+            <Link to="/total-races">Leaderboard</Link>
+          </CardStats>
+          {/* </Col> */}
+
+          {member.racesFull != member.racesTotal ? (
             <CardStats
-              title = 'Sprint+ Races'
-              info = 'Includes Sprint (previously Full) and Double Sprint events.'
-              value = {member.racesFull ? member.racesFull : '-'}
+              title="All* Races"
+              info="In addition to Sprint+, this includes Tempta (previously Intermediate), Aquabike, Swimrun, and Teams but records for these are not comprehensive."
+              value={member.racesTotal ? member.racesTotal : "-"}
             >
               <Link to="/total-races">Leaderboard</Link>
             </CardStats>
-          {/* </Col> */}
+          ) : (
+            ""
+          )}
 
-{member.racesFull != member.racesTotal ?
-
+          {member.committee[0] ? (
             <CardStats
-            title = 'All* Races'
-            info = 'In addition to Sprint+, this includes Tempta (previously Intermediate), Aquabike, Swimrun, and Teams but records for these are not comprehensive.'
-            value = {member.racesTotal ? member.racesTotal : '-'}
-            >
-              <Link to="/total-races">Leaderboard</Link>
-            </CardStats> : ""
-              }
-
-         {member.committee[0]  ?  <CardStats
-              title = "Committee"
-              value = {member.committee[0].count}
-              valueText = {member.committee[0].count > 1 ? " seasons" : " season"}
+              title="Committee"
+              value={member.committee[0].count}
+              valueText={member.committee[0].count > 1 ? " seasons" : " season"}
             >
               <Link to="/committee">View All</Link>
+            </CardStats>
+          ) : (
+            ""
+          )}
+        </Row>
 
-
-            </CardStats> : ""}
-            </Row>
-
-          {/* Change this to speciic flag */}
-          {member.plot.barData ? (member.plot.barData.length  ? (
-
-            
-
+        {/* Change this to speciic flag */}
+        {member.plot.barData ? (
+          member.plot.barData.length ? (
             <Row>
               <span>
-                  <span>
-
-                  <h2 style={{display: "inline-block"}}>
-                  {'Races 2024/25 onwards' + ' ' } 
-                <OverlayTrigger placement="top" overlay={popover}>
-                  <span style={{width: "fit-content"}}>
-                  &#9432;
-                </span>
-              </OverlayTrigger>
+                <span>
+                  <h2 style={{ display: "inline-block" }}>
+                    {"Races 2024/25 onwards" + " "}
+                    <OverlayTrigger placement="top" overlay={popover}>
+                      <span style={{ width: "fit-content" }}>&#9432;</span>
+                    </OverlayTrigger>
                   </h2>
-</span>
-                
+                </span>
               </span>
-              <MemberPlot
-                plotData={member.plot}/>
+              <MemberPlot plotData={member.plot} />
             </Row>
-              ) : ("")) : ("")
-}
-
-        
-
-
+          ) : (
+            ""
+          )
+        ) : (
+          ""
+        )}
 
         {member.notes && <p>{member.notes}</p>}
-
-
       </Container>
     </>
   );
-  }
+}
