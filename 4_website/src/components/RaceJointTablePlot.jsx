23dfa4fa191387f24747575b091fba6978f0f715
Joshua Horsley <24281654+joshhorsley@users.noreply.github.com>
2025-07-12 16:17:02 +1000

    For race pages, created joint plot and table component and lifted distance and category state management to this

---
+++ 4_website/src/components/RaceJointTablePlot.jsx
@@ -0,0 +1,91 @@
+import { SelectButton } from "primereact/selectbutton";
+import { Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+
+import { TabView, TabPanel } from "primereact/tabview";
+
+import RacePlot2 from "./RacePlot2";
+
+export default function RaceJointTablePlot({ plotData }) {
+  const [distance, setDistance] = useState("sprint");
+  const [category, setCategory] = useState("Handicap points");
+  const [categoryOptions, setCategoryOptions] = useState();
+
+  // distance
+  useEffect(() => {
+    const haveDistance =
+      plotData.distances.filter((e) => e.distanceID == distance).length == 1;
+
+    if (!haveDistance) {
+      const haveSprint =
+        plotData.distances.filter((e) => e.distanceID == "sprint").length == 1;
+
+      if (haveSprint) {
+        setDistance("sprint");
+      } else {
+        setDistance(plotData.distances[0].distanceID);
+      }
+    }
+  }, [plotData]);
+
+  // category
+  useEffect(() => {
+    if (plotData.categories) {
+      const catDistanceOptions = plotData.categories.filter(
+        (e) => e.distanceID == distance
+      );
+
+      if (catDistanceOptions.length) {
+        const haveCategory =
+          catDistanceOptions.filter((e) => e.Category == category).length == 1;
+
+        setCategoryOptions(catDistanceOptions);
+
+        if (!haveCategory) {
+          setCategory(catDistanceOptions[0].Category);
+        }
+      }
+    }
+  }, [plotData, distance, category]);
+
+  return (
+    <>
+      <Row>
+        <div style={{ float: "left" }}>
+          Distance
+          <SelectButton
+            allowEmpty={false}
+            value={distance}
+            options={plotData.distances}
+            optionValue="distanceID"
+            optionLabel="distanceDisplay"
+            onChange={(e) => setDistance(e.value)}
+          />
+          Category
+          <SelectButton
+            allowEmpty={false}
+            value={category}
+            options={categoryOptions}
+            optionValue="Category"
+            optionLabel="Category"
+            onChange={(e) => setCategory(e.value)}
+          />
+        </div>
+      </Row>
+      <TabView renderActiveOnly={false}>
+        <TabPanel header="Graph" leftIcon="pi pi-chart-bar mr-2">
+          {plotData && (
+            <RacePlot2
+              plotData={plotData}
+              distance={distance}
+              category={category}
+            />
+          )}
+        </TabPanel>
+        <TabPanel header="Table" leftIcon="pi pi-table mr-2">
+          {/* <MemberTable tabData={tabData} dataOption={dataOption} /> */}
+        </TabPanel>
+      </TabView>
+    </>
+  );
+}
--- 4_website/src/routes/race-instance.jsx
+++ 4_website/src/routes/race-instance.jsx
@@ -1,144 +1,113 @@
 // import modules ------------------------------------------------
 
+import { Link, useLoaderData } from "react-router-dom";
 
-import {
-  Link,
-    useLoaderData,
-} from "react-router-dom";
-
-import {Container, Row } from "react-bootstrap";
-
+import { Container, Row } from "react-bootstrap";
 
 // import components ------------------------------------------------
-
 
 import PageTitle from "../components/PageTitle";
 import TableTeamsManual from "../components/TableTeamsManual";
 import CardStats from "../components/cardstats";
-import RacePlot2 from "../components/RacePlot2";
-
+import RaceJointTablePlot from "../components/RaceJointTablePlot";
 
 // Component ------------------------------------------------
 
-
 export default function RaceInstance() {
-  const {race} = useLoaderData();
+  const { race } = useLoaderData();
 
   return (
     <>
-      <PageTitle title = {race.date_display ? race.date_display : null}/>
+      <PageTitle title={race.date_display ? race.date_display : null} />
 
       <Container>
         <h1 className="memberTitle">
-          {race.date_display ? (
-            <>
-              {race.date_display}
-            </>
-          ) : (
-            <i>No Name</i>
-          )}
-          
-          {/* Extra note */}
-          {race.extraNote ? (
-            <>{` ${race.extraNote}`}</>
-          ):("")}
+          {race.date_display ? <>{race.date_display}</> : <i>No Name</i>}
 
+          {/* Extra note */}
+          {race.extraNote ? <>{` ${race.extraNote}`}</> : ""}
         </h1>
 
         <Row>
           {race.marshalling.length ? (
-            <CardStats
-              title="Thanks to Marshals"  
-            >
+            <CardStats title="Thanks to Marshals">
               <ul>
-              {race.marshalling.map((e) => {
-
-                return(
-                <li key={e.id_member}>
-                  <Link
-                    to={`/members/${e.id_member}`}
-                  >
-                    {e.name_display}
-                  </Link>
-                </li>)
-              
-              })}
+                {race.marshalling.map((e) => {
+                  return (
+                    <li key={e.id_member}>
+                      <Link to={`/members/${e.id_member}`}>
+                        {e.name_display}
+                      </Link>
+                    </li>
+                  );
+                })}
               </ul>
             </CardStats>
-          ) : ("")}
-          
+          ) : (
+            ""
+          )}
+
           {race.newcomers.length ? (
-            <CardStats
-              title="First Timers"  
-            >
+            <CardStats title="First Timers">
               <ul>
-              {race.newcomers.map((e) => {
-
-                return(
-                <li key={e.id_member}>
-                  <Link
-                    to={`/members/${e.id_member}`}
-                  >
-                    {e.name_display}
-                  </Link>
-                </li>)
-              
-              })}
+                {race.newcomers.map((e) => {
+                  return (
+                    <li key={e.id_member}>
+                      <Link to={`/members/${e.id_member}`}>
+                        {e.name_display}
+                      </Link>
+                    </li>
+                  );
+                })}
               </ul>
             </CardStats>
-          ) : ("")}
-          
-          
+          ) : (
+            ""
+          )}
+
           {race.milestones.length ? (
-            <CardStats
-              title="Milestones"  
-            >
+            <CardStats title="Milestones">
               <ul>
-              {race.milestones.map((e) => {
-
-                return(
-                <li key={e.id_member}>
-                  <Link
-                    to={`/members/${e.id_member}`}
-                  >
-                    {e.name_display}
-                  </Link>{" "}{e.races_use}
-                </li>)
-              
-              })}
+                {race.milestones.map((e) => {
+                  return (
+                    <li key={e.id_member}>
+                      <Link to={`/members/${e.id_member}`}>
+                        {e.name_display}
+                      </Link>{" "}
+                      {e.races_use}
+                    </li>
+                  );
+                })}
               </ul>
             </CardStats>
-          ) : ("")}
-          
-        {race.raceStats.n_entered ? (
-          <CardStats
-          title="Finishers"
-          value={race.raceStats.n_entered}
-          />
-          
-          
-        ):("")}
-        {
-          race.raceStats.n_handicapAwards ? (
-            <CardStats
-            title="Handicaps Points"
-            value={race.raceStats.n_handicapAwards}
-            valueText="racers"
-            />
-          ) :("")
-        }
+          ) : (
+            ""
+          )}
 
+          {race.raceStats.n_entered ? (
+            <CardStats title="Finishers" value={race.raceStats.n_entered} />
+          ) : (
+            ""
+          )}
+          {race.raceStats.n_handicapAwards ? (
+            <CardStats
+              title="Handicaps Points"
+              value={race.raceStats.n_handicapAwards}
+              valueText="racers"
+            />
+          ) : (
+            ""
+          )}
         </Row>
 
-          {race.plot2 ? (<RacePlot2 plotData={race.plot2}/>):("no")}
+        {race.plot2 ? <RaceJointTablePlot plotData={race.plot2} /> : "no"}
 
-
-       
-
-        {race.extraNote=="Teams" ? (<TableTeamsManual tabData = {race.tabData}/>) : ("")}
-
+        {race.extraNote == "Teams" ? (
+          <TableTeamsManual tabData={race.tabData} />
+        ) : (
+          ""
+        )}
       </Container>
-</>
+    </>
   );
-
 }
