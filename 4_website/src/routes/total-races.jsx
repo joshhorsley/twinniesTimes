29b2bd7965eff8ba9278c7c16f6ce61ed2206c7a
Joshua Horsley <24281654+joshhorsley@users.noreply.github.com>
2025-07-13 17:36:46 +1000

    Encapsulated total races table in separate component

--- 4_website/src/routes/total-races.jsx
+++ 4_website/src/routes/total-races.jsx
@@ -1,61 +1,11 @@
-import { DataTable } from "primereact/datatable";
-import { Column } from "primereact/column";
-
-import { Form } from "react-router-dom";
-
 import dataTotalRaces from "../data/totalRaces.json";
-import { Container, PopoverBody } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { useState } from "react";
-
-import { FilterMatchMode } from "primereact/api";
+import { Container } from "react-bootstrap";
 
 import PageTitle from "../components/PageTitle";
 
-import { Popover, OverlayTrigger } from "react-bootstrap";
+import TotalRacesTable from "../components/TotalRacesTables";
 
 export default function TotalRaces() {
-  const [filters, setFilters] = useState({
-    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-  });
-
-  const [globalFilterValue, setGlobalFilterValue] = useState("");
-
-  const onGlobalFilterChange = (e) => {
-    const value = e.target.value;
-    let _filters = { ...filters };
-
-    _filters["global"].value = value;
-
-    setFilters(_filters);
-    setGlobalFilterValue(value);
-  };
-
-  const renderHeader = () => {
-    return (
-      <div>
-        <Form id="search-form" role="search">
-          <input
-            id="qTab"
-            name="q"
-            type="search"
-            value={globalFilterValue}
-            onChange={onGlobalFilterChange}
-            spellCheck="false"
-            aria-label="Search by name"
-            placeholder="Search by name"
-          />
-          <div className="sr-only" aria-live="polite"></div>
-        </Form>
-      </div>
-    );
-  };
-
-  const header = renderHeader();
-
-  // example of sticky header
-  // https://codesandbox.io/p/sandbox/flamboyant-dawn-nvd7oc?file=%2Fsrc%2Fdemo%2FDataTableDemo.css%3A38%2C1-61%2C1
-
   return (
     <>
       <PageTitle title="Total Races" />
@@ -63,72 +13,7 @@ export default function TotalRaces() {
       <div className="contentScroll">
         <Container>
           <h1>Total races as of {dataTotalRaces.dateUpdated}</h1>
-
-          <DataTable
-            value={dataTotalRaces.totalData}
-            size="small"
-            // for search
-            filters={filters}
-            filterDisplay="row"
-            globalFilterFields={["name_display"]}
-            header={header}
-            // sorting
-            sortField="races_full"
-            sortOrder={-1}
-            stripedRows
-            paginator
-            rows={25}
-            rowsPerPageOptions={[5, 10, 25, 50]}
-            scrollable
-          >
-            {/* Name */}
-            <Column
-              field="name_display"
-              header="Name"
-              body={(dataTotalRaces) => {
-                return (
-                  <Link to={`/members/${dataTotalRaces.id_member}`}>
-                    {dataTotalRaces.name_display}
-                  </Link>
-                );
-              }}
-              sortable
-              frozen
-              style={{ minWidth: "100px" }}
-            />
-
-            {dataTotalRaces.colsUse.map((col) => (
-              <Column
-                key={col.columnID}
-                field={col.columnID}
-                sortable
-                style={{ minWidth: "100px", textAlign: "left" }}
-                header={
-                  col.note ? (
-                    <>
-                      {
-                        <OverlayTrigger
-                          placement="top"
-                          overlay={
-                            <Popover>
-                              <PopoverBody>{col.note}</PopoverBody>
-                            </Popover>
-                          }
-                        >
-                          <span>
-                            {col.title + " "}
-                            &#9432;
-                          </span>
-                        </OverlayTrigger>
-                      }
-                    </>
-                  ) : (
-                    col.title
-                  )
-                }
-              />
-            ))}
-          </DataTable>
+          <TotalRacesTable tabDataList={dataTotalRaces} />
         </Container>
       </div>
     </>
