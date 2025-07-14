a81cda5e42095777f833799922f1fdd77cb07ff0
Joshua Horsley <24281654+joshhorsley@users.noreply.github.com>
2025-07-14 15:23:13 +1000

    Generalised committee table component to people table

---
+++ 4_website/src/components/PeopleTable.jsx
@@ -0,0 +1,71 @@
+//  Packages --------------------------------------------------------
+
+import { Link } from "react-router-dom";
+
+import { useEffect, useState } from "react";
+
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+
+//  Data --------------------------------------------------------
+
+import { intersperse } from "../intersperse";
+
+//  Component --------------------------------------------------------
+
+export default function PeopleTable({ dataPeople }) {
+  const [tableStart, setTableStart] = useState({
+    // start: document.getElementById("tableCommittee").offsetTop
+    start: 141,
+  });
+
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <DataTable
+      id="tableCommittee"
+      value={dataPeople.tab.data}
+      // sorting
+      sortField="seasonDisplay"
+      sortOrder={-1}
+      scrollable
+      scrollHeight={`${windowDimensions.height - tableStart.start - 10}px`}
+    >
+      <Column field="seasonDisplay" header="Season" sortable frozen />
+
+      {dataPeople.tab.colDefs.map((col) => (
+        <Column
+          key={col.columnID}
+          // field="season"
+          header={col.title}
+          body={(e) => {
+            const tags = e[`${col.title}`].map((e2, i) => {
+              return (
+                <Link key={`${col.title}-${i}`} to={`/members/${e2.id_member}`}>
+                  {e2.name_display}
+                </Link>
+              );
+            });
+
+            return intersperse(tags, ", ");
+          }}
+        />
+      ))}
+    </DataTable>
+  );
+}
--- 4_website/src/routes/committee.jsx
+++ 4_website/src/routes/committee.jsx
@@ -1,45 +1,28 @@
 //  Packages --------------------------------------------------------
 
-
 import { Container } from "react-bootstrap";
 
-
-import CommitteeTable from "../components/CommitteeTable";
+import PeopleTable from "../components/PeopleTable";
 import PageTitle from "../components/PageTitle";
-
 
 //  Data --------------------------------------------------------
 
-// import dataCommittee from "../data/committee.json"
+import dataCommittee from "../data/committee.json";
 
 //  Component --------------------------------------------------------
 
-
 export default function Committee() {
-
-
-return(
+  return (
     <>
-            <PageTitle title = "Committee"/>
+      <PageTitle title="Committee" />
 
-
-<div className="contentScroll">
-
-    <Container>
-
-
-<h1>Committee</h1>
-
-Some incomplete records between 1995/96 and 2002/03.
-
-
-<CommitteeTable/>
-
-    </Container>
-
-</div>
+      <div className="contentScroll">
+        <Container>
+          <h1>Committee</h1>
+          Some incomplete records between 1995/96 and 2002/03.
+          <PeopleTable dataPeople={dataCommittee} />
+        </Container>
+      </div>
     </>
-
-)
-
+  );
 }
